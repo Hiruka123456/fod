65d3522e217e2961efa546ab832dacf07828ffa7
Hiruka123456 <134048711+Hiruka123456@users.noreply.github.com>
2024-12-03 23:43:38 +0530

    Add files via upload

---
+++ commands/info/ping.js
@@ -0,0 +1,16 @@
+const {MessageEmbed } = require('discord.js')
+const Discord = require('discord.js')
+module.exports = {
+    name: 'ping',
+    category:"info",
+    description: 'Get bot\'s ping',
+    usage: 'ping',
+    timeout: "2000",
+    run : async (client, message, args) => {
+        message.channel.send('Pinging...')
+        .then(sentMsg => {
+            sentMsg.edit(`:ping_pong: Pong! Took \`${sentMsg.createdTimestamp - message.createdTimestamp}ms\` | Latency- \`${client.ws.ping}ms\``)
+   })
+
+}
+}
